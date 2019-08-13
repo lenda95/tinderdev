cd554a1cf33aa9a5ec3954784c0edacd8bb943d3
lenda95 <vidalcg95@gmail.com>
2019-08-13 17:32:35 -0300

    incluindo CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const server = express();
 
 mongoose.connect('mongodb+srv://vidaldev:swordfish95@cluster0-lfvyw.mongodb.net/test?retryWrites=true&w=majority',
     {useNewUrlParser: true}
 );
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
